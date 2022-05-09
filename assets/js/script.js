49961176094ad92662d59b932eff85d6fc2a4879
Folkor1 <igorvasiljevs@gmail.com>
2022-05-09 19:32:18 +0000

    Add function that validates the input value

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let nameHeading = document.getElementById("name-div");
+let input = document.getElementById("name-input");
+let start = document.getElementById("start-button");
+let quizGame = document.getElementById("quiz-div");
+
+function inputStart() {
+    if (input.value.match(/^[0-9A-Za-z]+$/) !== null) {
+        input.style.display = "none";
+        start.style.display = "none";
+        nameHeading.style.display = "none";
+        quizGame.style.display = "block";
+    } else {
+        alert("test");
+    }
+}
+start.addEventListener('click', inputStart);
